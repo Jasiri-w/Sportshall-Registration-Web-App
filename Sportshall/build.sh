b5b9d608cd71d8338bf07a1d6bcd9b65a75347bb
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2023-10-07 12:44:54 -0400

    Included build logs to notify us when it has been ran by Vercel
    Debugging things uno

--- Sportshall/build.sh
+++ Sportshall/build.sh
@@ -1,6 +1,9 @@
 # build_files.sh
+echo "Build started at $(date) ~ from Jasiri"
 pip -r requirements.txt
 
 # make migrations
 python manage.py migrate
 python manage.py collectstatic
+
+echo "Build completed at $(date) ~ Kwaheri"
