6c26cddd08712581c5315d6870d898517e3e7992
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2023-10-08 14:17:52 -0400

    Added auto registration of models to admin

--- Sportshall/altFrontend/admin.py
+++ Sportshall/altFrontend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+alt_frontend_models = apps.get_app_config('altFrontend').get_models()
+
+for model in alt_frontend_models:
+    admin.site.register(model)
