3ed77efb3eccae31edd84e08068d4518edb07515
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2023-10-08 14:45:47 -0400

    Removed need for input on collect static

--- Sportshall/build_files.sh
+++ Sportshall/build_files.sh
@@ -4,6 +4,6 @@ pip install -r requirements.txt
 
 # make migrations
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --no-input # Flag to force confirmation
 
 echo "Build completed at $(date) ~ Kwaheri"
