b08feeaf61458cadc31f5bc5ad6fd82675d934b1
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2023-10-09 15:56:40 -0400

    Update build_files.sh
    Added provisioning for automated creation of a super user using environment variables for credentials, with checks that the user does not already exist.

--- Sportshall/build_files.sh
+++ Sportshall/build_files.sh
@@ -1,9 +1,19 @@
-# build_files.sh
 echo "Build started at $(date) ~ from Jasiri"
 pip install -r requirements.txt
 
-# make migrations
+# Make migrations and migrate
 python3.9 manage.py migrate
+
+# Check if the superuser already exists
+if [ -z "$(python3.9 manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists())')" ]; then
+    # Superuser doesn't exist, create one
+    echo "Creating superuser..."
+    python3.9 manage.py createsuperuser --no-input
+else
+    # Superuser already exists, skip creation
+    echo "Superuser already exists. Skipping creation."
+fi
+
 python3.9 manage.py collectstatic --no-input # Flag to force confirmation
 
 echo "Build completed at $(date) ~ Kwaheri"
